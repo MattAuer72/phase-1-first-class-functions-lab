b21e238cb3dc7951551697c5e1ace369bb577a7c
Matthew Auer <matthewauer72@gmail.com>
2023-04-25 14:00:59 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers = () => drivers.slice(0,2);   
+const returnLastTwoDrivers = () => drivers.slice(2,4);
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = (num) => {
+  const fareMultiplier = () => num * num
+
+  return fareMultiplier
+}  
+    
+    
+ 
+   function fareDoubler(fareMultiplier) {
+    return fareMultiplier * 2
+  }
+    
+  function fareTripler(fareMultiplier) {
+    return fareMultiplier * 3
+  }
+
+  const selectDifferentDrivers = (array, functionName) => {
+    return functionName(array)
+
+  }
+  
+
+
+ 
+
